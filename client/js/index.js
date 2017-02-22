50f7b30319f61b9ee97cacce6f7d13f1faac65d5
Laurent <lau.cazanove@gmail.com>
2017-02-22 21:31:13 +0100

    Signature copy to clipboard implemented and information panel hide/show

--- client/js/index.js
+++ client/js/index.js
@@ -1,18 +1,47 @@
 var app = new Vue({
 	
 	el: '#app',
+
+	mounted: function () {
+		var clipboard = new Clipboard('#copy-button');
+		clipboard.on('success', function(e) {
+		    e.clearSelection();
+		});
+	},
 	
 	data: {
 		name: 'John',
 		surname: 'Doe',
 		position: 'CEO',
 		phone: '00 00 00 00 00',
-		email: 'john.doe@mail.com'
+		email: 'john.doe@mail.com',
+		linkedinUrl: 'https://fr.linkedin.com/in/laurentcazanove',
+		twitterUrl: 'https://twitter.com/lau_cazanove',
+		informationPanel: {
+			visible: true,
+			toggleButtonText: 'Hide form'
+		}
 	},
 	
 	computed: {
 		mailToEmail: function() {
-			return 'mailto:' + this.email
+			return 'mailto:' + this.email;
+		},
+
+		portraitTitle: function() {
+			return 'Picture of ' + this.name + " " + this.surname;
+		}
+	},
+
+	methods: {
+		toggleInformationPanel: function() {
+			if (this.informationPanel.visible) {
+				this.informationPanel.toggleButtonText = "Show form";
+			} else {
+				this.informationPanel.toggleButtonText = "Hide form";
+			}
+			this.informationPanel.visible = !this.informationPanel.visible;
 		}
 	}
-})
+});
+
