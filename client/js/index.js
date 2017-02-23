1d6eca40d67f2f667a8bec0f7f728b2d6e8cea0f
Laurent <lau.cazanove@gmail.com>
2017-02-23 02:50:56 +0100

    Information can be add to or removed from signature

--- client/js/index.js
+++ client/js/index.js
@@ -3,23 +3,68 @@ var app = new Vue({
 	el: '#app',
 
 	mounted: function () {
+		var vue = this;
 		var clipboard = new Clipboard('#copy-button');
 		clipboard.on('success', function(e) {
 		    e.clearSelection();
+		    vue.copiedSignature = true;
 		});
 	},
 	
 	data: {
-		name: 'John',
-		surname: 'Doe',
-		position: 'CEO',
-		phone: '00 00 00 00 00',
-		email: 'john.doe@mail.com',
-		linkedinUrl: 'https://fr.linkedin.com/in/laurentcazanove',
-		twitterUrl: 'https://twitter.com/lau_cazanove',
+		user: {
+			name: 'John',
+			surname: 'DOE',
+			position: 'CEO',
+			phone: '00 00 00 00 00',
+			email: 'john.doe@mail.com',
+			linkedinUrl: 'https://fr.linkedin.com/in/laurentcazanove',
+			twitterUrl: 'https://twitter.com/lau_cazanove'
+		},
 		informationPanel: {
 			visible: true,
 			toggleButtonText: 'Hide form'
+		},
+		copiedSignature: false,
+		signature: {
+			surnameToUpperCase: true,
+			enablePosition: true,
+			enablePhone: false,
+			enableLinkedin: false,
+			enableTwitter: false
+		}
+	},
+
+	watch: {
+		'user.name': function(newValue) {
+			this.copiedSignature = false;
+			this.user.name = newValue.charAt(0).toUpperCase() + newValue.slice(1);
+		},
+		'user.surname': function(newValue) {
+			this.copiedSignature = false;
+			if (this.signature.surnameToUpperCase) {
+				this.user.surname = newValue.toUpperCase();
+			}
+		},
+		'user.position': function(newValue) {
+			this.copiedSignature = false;
+		},
+		'user.phone': function(newValue) {
+			this.copiedSignature = false;
+		},
+		'user.email': function(newValue) {
+			this.copiedSignature = false;
+		},
+		'user.linkedinUrl': function(newValue) {
+			this.copiedSignature = false;
+		},
+		'user.twitterUrl': function(newValue) {
+			this.copiedSignature = false;
+		},
+		'signature.surnameToUpperCase': function(newValue) {
+			if (newValue) {
+				this.user.surname = this.user.surname.toUpperCase();
+			}
 		}
 	},
 	
@@ -30,7 +75,7 @@ var app = new Vue({
 
 		portraitTitle: function() {
 			return 'Picture of ' + this.name + " " + this.surname;
-		}
+		},
 	},
 
 	methods: {
@@ -41,6 +86,26 @@ var app = new Vue({
 				this.informationPanel.toggleButtonText = "Hide form";
 			}
 			this.informationPanel.visible = !this.informationPanel.visible;
+		},
+
+		togglePosition: function() {
+			this.signature.enablePosition = !this.signature.enablePosition;
+		},
+
+		toggleLinkedin: function() {
+			this.signature.enableLinkedin = !this.signature.enableLinkedin;
+		},
+
+		toggleTwitter: function() {
+			this.signature.enableTwitter = !this.signature.enableTwitter;
+		},
+
+		togglePhone: function() {
+			this.signature.enablePhone = !this.signature.enablePhone;
+		},
+
+		click: function(event) {
+			console.log(event);
 		}
 	}
 });
